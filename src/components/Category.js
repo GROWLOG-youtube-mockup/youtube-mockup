bd2fe03e2e6999eabbd2966b8e71cd88d56a3d3b
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-04 23:00:40 +0900

    refactor: add entry point

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,1 +1,12 @@
-export default class Category {}
+class Category {
+  constructor() {
+    this.message = 'Category';
+    this.test();
+  }
+
+  test() {
+    console.log(this.message);
+  }
+}
+
+export default Category;

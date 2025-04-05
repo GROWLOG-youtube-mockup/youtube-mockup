9e7e109e4f8b1718e73307058d1cb15aaa1693c8
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-05 11:07:20 +0900

    feat: add category element

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,74 @@
+import { createElement } from '../utils/util.js';
+
 class Category {
   constructor() {
     this.message = 'Category';
-    this.test();
+    this.categoryDiv = document.getElementById('chip-categories');
+    this.categories = [
+      '전체',
+      '게임',
+      '음악',
+      '영화',
+      '애니메이션',
+      '스포츠',
+      '뉴스',
+      '교양',
+      '브이로그',
+      '쇼츠'
+    ]; // 카테고리 목록
+    this.chips = [];
+    this.selectedCategory = null;
+
+    this.init();
+    this.addDragging();
   }
 
-  test() {
-    console.log(this.message);
+  init() {
+    this.categories.forEach((category, index) => {
+      const div = createElement('div', this.categoryDiv, {
+        className: 'chip',
+        innerText: category,
+        classList: {
+          method:
+            this.selectedCategory === category || (!this.selectedCategory && index === 0)
+              ? 'add'
+              : 'remove',
+          className: ['chip_active']
+        }
+      });
+    });
+  }
+
+  addDragging() {
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    this.categoryDiv.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startX = e.pageX - this.categoryDiv.offsetLeft;
+      scrollLeft = this.categoryDiv.scrollLeft;
+    });
+
+    this.categoryDiv.addEventListener('mouseup', () => {
+      isDragging = false;
+    });
+
+    this.categoryDiv.addEventListener('mousemove', (e) => {
+      if (!isDragging) {
+        return;
+      }
+
+      e.preventDefault();
+
+      const x = e.pageX - this.categoryDiv.offsetLeft;
+      const walk = (x - startX) * 1.5;
+      this.categoryDiv.scrollLeft = scrollLeft - walk;
+    });
+
+    this.categoryDiv.addEventListener('mouseleave', () => {
+      isDragging = false;
+    });
   }
 }
 

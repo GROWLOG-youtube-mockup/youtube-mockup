4860fbfb5c1189c009b402d98395c8d77c434cc3
Jammanb0 <parkguhn35@gmail.com>
2025-04-15 15:50:35 +0900

    refactor: Sidebar.js, Sidebar.css refactor 진행

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,96 +1,71 @@
 class Sidebar {
-    constructor() {
-      this.render();
-      this.addEventListeners();
-    }
-  
-    render() {
-      const sidebar = document.querySelector('.sidebar');
-      sidebar.innerHTML = `
-        <nav class="sidebar__nav">
-          <ul class="sidebar__menu">
-            <li class="sidebar__item">
-              <a href="#" class="sidebar__link">
-                <img src="./assets/icons/home.svg" alt="Home" class="sidebar__icon" />
-                <span>홈</span>
-              </a>
-            </li>
-            <li class="sidebar__item">
-              <a href="#" class="sidebar__link">
-                <img src="./assets/icons/subscriptions.svg" alt="subscriptions" class="sidebar__icon" />
-                <span>구독</span>
-              </a>
-            </li>
-            <li class="sidebar__item">
-              <a href="#" class="sidebar__link">
-                <img src="./assets/icons/youtube-music.svg" alt="youtube-music" class="sidebar__icon" />
-                <span>YouTube Music</span>
-              </a>
-            </li>
-            
-            <li><hr class="sidebar__divider" /></li>
+  constructor() {
+    this.sidebarItems = [
+      { icon: 'home', text: '홈' },
+      { icon: 'subscriptions', text: '구독' },
+      { icon: 'youtube-music', text: 'YouTube Music' },
+      { divider: true },
+      { icon: 'history', text: '시청 기록' },
+      { icon: 'playlist', text: '재생목록' },
+      { icon: 'smart-display', text: '내 동영상' },
+      { icon: 'schedule', text: '나중에 볼 동영상' },
+      { icon: 'thumb-up', text: '좋아요 표시한 동영상' },
+      { divider: true },
+      { icon: 'popularity', text: '인기 급상승' },
+      { icon: 'music-note', text: '음악' }
+    ];
 
-            <li class="sidebar__item">
-              <a href="#" class="sidebar__link">
-                <img src="./assets/icons/history.svg" alt="history" class="sidebar__icon" />
-                <span>시청 기록</span>
-              </a>
-            </li>
-            <li class="sidebar__item">
-              <a href="#" class="sidebar__link">
-                <img src="./assets/icons/playlist.svg" alt="playlist" class="sidebar__icon" />
-                <span>재생목록</span>
-              </a>
-            </li>
-            <li class ="sidebar__item">
-                <a href="#" class="sidebar__link">
-                    <img src="./assets/icons/smart-display.svg" alt="smart-display" class="sidebar__icon" />
-                    <span> 내 동영상 </span>
-                </a>
-            </li>
-            <li class ="sidebar__item">
-                <a href="#" class="sidebar__link">
-                    <img src="./assets/icons/schedule.svg" alt="schedule" class="sidebar__icon" />
-                    <span> 나중에 볼 동영상 </span>
-                </a>
-            </li>
-            <li class ="sidebar__item">
-                <a href="#" class="sidebar__link">
-                    <img src="./assets/icons/thumb-up.svg" alt="thumb-up" class="sidebar__icon" />
-                    <span> 좋아요 표시한 동영상  </span>
-                </a>
-            </li>
-
-            <li><hr class="sidebar__divider" /></li>
-
-            <li class ="sidebar__item">
-                <a href="#" class="sidebar__link">
-                    <img src="./assets/icons/popularity.svg" alt="popularity" class="sidebar__icon" />
-                    <span> 인기 급상승 </span>
-                </a>
-            </li>
-            <li class ="sidebar__item">
-                <a href="#" class="sidebar__link">
-                    <img src="./assets/icons/music-note.svg" alt="music-note" class="sidebar__icon" />
-                    <span> 음악 </span>
-                </a>
-            </li>
-          </ul>
-        </nav>
-      `;
-    }
-
-    addEventListeners() {
-        const items = document.querySelectorAll('.sidebar__item');
-    
-        items.forEach((item) => {
-          item.addEventListener('click', () => {
-            items.forEach((sidebarItem) => sidebarItem.classList.remove('sidebar__item--active'));
-    
-            item.classList.add('sidebar__item--active');
-          });
-        });
-      }
+    this.render();
+    this.addEventListeners();
   }
-  
-  export default Sidebar;
+
+  createMenuItem(item) {
+    if (item.divider) {
+      return '<li><hr class="sidebar__divider" /></li>';
+    }
+
+    return `
+          <li class="sidebar__item">
+              <a href="#" class="sidebar__link">
+                  <img 
+                      src="./assets/icons/${item.icon}.svg" 
+                      alt="${item.text}" 
+                      class="sidebar__icon" 
+                  />
+                  <span>${item.text}</span>
+              </a>
+          </li>
+      `;
+  }
+
+  render() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.innerHTML = `
+          <nav class="sidebar__nav" aria-label="Main navigation">
+              <ul class="sidebar__menu">
+                  ${this.sidebarItems.map((item) => this.createMenuItem(item)).join('')}
+              </ul>
+          </nav>
+      `;
+  }
+
+  addEventListeners() {
+    const items = document.querySelectorAll('.sidebar__item');
+
+    items.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.setActiveItem(item);
+      });
+    });
+  }
+
+  setActiveItem(activeItem) {
+    document
+      .querySelectorAll('.sidebar__item')
+      .forEach((item) => item.classList.remove('sidebar__item--active'));
+    activeItem.classList.add('sidebar__item--active');
+  }
+}
+
+export default Sidebar;
